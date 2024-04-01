a465e3b081e301bb14e43c504f05ce7d4c70a10d
Jakob Lewis <149333423+auxpatch@users.noreply.github.com>
2024-04-01 00:46:56 -0400

    It's All Done!
    YAY!

--- TaskOne.cs
+++ TaskOne.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class TaskOne : MonoBehaviour
 {
     private Queue<string> playerQueue = new Queue<string>();
-    private int playerCount = 26;
-    public void AddPlayers()
+    public int playerCount;
+
+    void Start()
     {
         playerQueue.Enqueue("Son Goku");
         playerQueue.Enqueue("Celyca");
@@ -35,16 +36,26 @@ public class TaskOne : MonoBehaviour
         playerQueue.Enqueue("Gwyneth");
         playerQueue.Enqueue("Anji");
         playerQueue.Enqueue("A.B.A.");
-    }
-
-    void Start()
-    {
-        AddPlayers();
-
-        string login = playerQueue.Dequeue();
 
         Debug.Log("Initial login queue created. There are " + playerCount + " players in the queue.");
-        Debug.Log(login);
+
+        if (playerCount == 0)
+        {
+            Debug.Log("There is no one in queue!");
+        }
+        else if(playerCount > 26)
+        {
+            Debug.Log("There's too many login attempts currently. Please try again later.");
+        }
+        else
+        {
+            for (int i = 0; i < playerCount; i++)
+            {
+                Debug.Log(playerQueue.Dequeue() + " is now inside the game.");
+                Debug.Log(playerQueue.Peek() + " is trying to login and is added to the login queue.");
+            }
+        }
+
 
     }
 
